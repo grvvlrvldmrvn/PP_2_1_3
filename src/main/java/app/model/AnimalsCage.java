4f6f95321d4ec80c090e3941fc3c6ccc2a7571e2
Ivan Puryakov <puryakov@vk.com>
2025-01-27 17:10:26 +0300

    Task done
    Create an object from Dog, create an object from Timer, and link them to AnimalCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private final Timer timer;
+
     @Autowired
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
