8c2f4c100a606165b54622db4eca79d86dd9ecb0
Ivan Puryakov <puryakov@vk.com>
2025-01-27 18:13:02 +0300

    Created a bean from Timer and linked it to AnimalCage

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,21 @@
 package app.config;
 
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Timer timer() {
+        return new Timer();
+    }
+
+    @Bean
+    public AnimalsCage animalsCage(Timer timer) {
+        return new AnimalsCage(timer);
+    }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,5 @@
 package app.model;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
